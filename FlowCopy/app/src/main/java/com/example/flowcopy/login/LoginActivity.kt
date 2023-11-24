5d2972f73947f879d273cfce5633d14d91ddc5e3
c-Alvinn <141906500+c-Alvinn@users.noreply.github.com>
2023-11-24 12:10:43 -0300

    Adding PopUp on LoginActivity and fixing activity_register

--- FlowCopy/app/src/main/java/com/example/flowcopy/login/LoginActivity.kt
+++ FlowCopy/app/src/main/java/com/example/flowcopy/login/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.EditText
 import com.example.flowcopy.DAO.Conta
 import com.example.flowcopy.DAO.DAO_Conta
@@ -33,8 +34,19 @@ class LoginActivity : AppCompatActivity() {
             //Validar login.
             val username = binding.editTextUsername.text.toString()
             val password = binding.editTextPassword.text.toString()
-            operacoesBanco.validarLogin(username,password)
-            limparCampos(binding.editTextUsername, binding.editTextPassword)
+            if (operacoesBanco.validarLogin(username,password)) {
+                limparCampos(binding.editTextUsername, binding.editTextPassword)
+                val msg = "Olá $username!\nSeja bem-vindo!"
+                binding.popUp.text = msg
+                binding.popUp.visibility = View.VISIBLE
+                binding.popUp.postDelayed({binding.popUp.visibility = View.INVISIBLE},2000)
+            }else {
+                val msg = "Login inválido.\nVerifique suas credenciais e tente novamente."
+                binding.popUp.text = msg
+                binding.popUp.visibility = View.VISIBLE
+                binding.popUp.postDelayed({binding.popUp.visibility = View.INVISIBLE},2000)
+            }
+
         }
 
         binding.buttonRegister.setOnClickListener {
